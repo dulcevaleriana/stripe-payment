331f2ed03dbb19596ef772f34565c0cf41ff6edb
dulce-guzman-azumo <dulce.guzman@azumo.co>
2023-06-07 18:07:30 -0400

    done first proof stripe normal buy product

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,49 @@
 "use client"
-import React from "react"
+import React, { useState } from "react"
 import Image from "next/image"
 import Product from "../image/img.jpg"
 import { loadStripe } from '@stripe/stripe-js'
-import { Elements, useElements, CardElement, useStripe } from "@stripe/react-stripe-js"
+import {
+  Elements,
+  useElements,
+  CardElement,
+  useStripe,
+} from "@stripe/react-stripe-js"
+import axios from "axios"
 
 const stripePromise = loadStripe("pk_test_51NGPLmHxcJiCMySE7kKST0V29XeL4pz1qtXw6sKmmp31c11mnvwSpeP8tK4OtGy0NmkV7Erp3yTkHBT6UbMKmnLm00Ap6A1kRe")
 
 const CheckoutForm = () => {
   const stripe = useStripe();
-  const elements = useElements()
+  const elements = useElements();
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
-      type: 'card',
-      card: elements.getElement(CardElement)
-    })
+    try {
+      const {error, paymentMethod} = await stripe.createPaymentMethod({
+        type: 'card',
+        card: elements.getElement(CardElement)
+      })
+
+      setLoading(true)
+
+      if(!error) {
+        console.log(paymentMethod)
+        const { id } = paymentMethod;
+
+        const {data} = await axios.post("http://localhost:3001/api/checkout",{
+          id,
+          amount: 20000
+        })
+        console.log(data)
+        elements.getElement(CardElement).clear();
+      }
+    } catch(err) {
+      console.log(err)
+    }
+    setLoading(false)
   }
 
   return <form onSubmit={handleSubmit} className="class-form">
@@ -25,7 +51,7 @@ const CheckoutForm = () => {
     <h2>Product</h2>
     <h4>price: USD$200</h4>
     <CardElement/>
-    <button>Buy</button>
+    <button>{loading ? "...": "Buy"}</button>
   </form>
 }
 
